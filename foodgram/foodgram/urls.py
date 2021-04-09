035f0a129c4eb841aef4f5fabc1ca62a110c0f24
Dmitrii Koshelev <me@koshelev.net>
2021-04-09 11:20:07 +0300

    Set up static

--- foodgram/foodgram/urls.py
+++ foodgram/foodgram/urls.py
@@ -1,8 +1,15 @@
 """foodgram URL Configuration."""
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from foodgram import settings
 
 urlpatterns = [
     path('', include('recipes.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
