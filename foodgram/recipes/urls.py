e065e23169cc0f05587e9a84e2200fbfe520caf0
Dmitrii Koshelev <me@koshelev.net>
2021-04-12 16:46:36 +0300

    Set up new recipe page

--- foodgram/recipes/urls.py
+++ foodgram/recipes/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:username>/<slug:slug>/',
+    path('<str:username>/recipe/<slug:slug>/',
          views.recipe_view,
          name='recipe'),
+    path('new-recipe/',
+         views.new_recipe,
+         name='new_recipe'),
+    path('ingredients/',
+         views.get_ing),
 ]
--- foodgram/recipes/views.py
+++ foodgram/recipes/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 
-from recipes.models import Recipe, User
+from recipes.models import Recipe, User, Ingredient
 
 
 def index(request):
@@ -31,12 +32,19 @@ def recipe_view(request, username, slug):
 
 @login_required
 def new_recipe(request):
-    new_post_form = PostForm(request.POST or None)
-    if not new_post_form.is_valid():
-        return render(request, 'posts/new_post.html', {'form': new_post_form})
+    # new_post_form = PostForm(request.POST or None)
+    # if not new_post_form.is_valid():
+    #     return render(request, 'posts/new_post.html', {'form': new_post_form})
+    #
+    # post = new_post_form.save(commit=False)
+    # post.author = request.user
+    # post.save()
+    # return redirect(reverse('index'))
+    return render(request, 'recipes/new_recipe.html', {})
 
-    post = new_post_form.save(commit=False)
-    post.author = request.user
-    post.save()
-    return redirect(reverse('index'))
-
+def get_ing(request):
+    query = request.GET.get('query', None)
+    if query:
+        result = Ingredient.objects.all().filter(name__startswith=query)
+    print(result)
+    return JsonResponse(list(result), safe=False)
