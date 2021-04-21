749be7533e96d12643a6fff1a18157386ba5b550
Dmitrii Koshelev <me@koshelev.net>
2021-04-21 22:05:38 +0300

    Refresh requirements

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,28 +1,20 @@
-"""API app URL Configuration."""
+"""`api` app URL Configuration."""
 from django.urls import include, path
-# from rest_framework.routers import DefaultRouter
-# from rest_framework_simplejwt.views import (TokenObtainPairView,
-#                                             TokenRefreshView)
-#
-# from users.views import UserViewSet, api_user_create, send_token
-#
-# users_router = DefaultRouter()
-# users_router.register('users', UserViewSet)
-#
+
 from rest_framework.routers import DefaultRouter
 
 from . import views
-from .views import FavoritesView, FollowView, PurchaseView
 
 router = DefaultRouter()
 
-router.register('favorites', FavoritesView)
-router.register('purchases', PurchaseView)
-router.register('subscriptions', FollowView)
+router.register('favorites', views.FavoritesView)
+router.register('purchases', views.PurchaseView)
+router.register('subscriptions', views.FollowView)
 
 urlpatterns_api_v1 = [
     path('', include(router.urls)),
-    path('ingredients/', views.api_get_ingredients,
+    path('ingredients/',
+         views.api_get_ingredients,
          name='get_ingredients'),
 ]
 
