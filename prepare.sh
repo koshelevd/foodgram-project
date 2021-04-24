a19e62c8ba78e4646297195e0cf2d7c4546fccaa
Dmitrii Koshelev <me@koshelev.net>
2021-04-24 18:40:00 +0300

    Fix entrypoint

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations users
+python manage.py migrate users
+python manage.py makemigrations about
+python manage.py migrate about
+python manage.py makemigrations api
+python manage.py migrate api
+python manage.py makemigrations recipes
+python manage.py migrate recipes
 python manage.py makemigrations
 python manage.py migrate
 
